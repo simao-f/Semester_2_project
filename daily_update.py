59753a8d30f205c28e8f5cf514e0778f1842c77d
simao-f <pt.simaoferreira@gmail.com>
2024-05-04 12:10:47 +0200

    daily update test 2

--- daily_update.py
+++ daily_update.py
@@ -8,7 +8,6 @@ import pandas as pd
 from sklearn.model_selection import cross_val_score
 
 import polygon 
-import hopsworks
 from datetime import datetime
 import time
 
@@ -47,26 +46,7 @@ def get_stock_from_api(stocksymbol):
     
     
 
-def upload_to_hopsworks(df,project):
-    
-    fs = project.get_feature_store()
-    
-    stock_fg = fs.get_or_create_feature_group(
-        name="daily_stock_price",
-        version=1,
-        description="",
-        primary_key=["date", "symbol"],
-        event_time="date",
-    )   
-    
-    stock_fg.insert(df)
-    
-def retrieve_model_from_hopsworks(project, stocksymbol):
-    mr = project.get_model_registry()
-    stock_model = mr.get_model("regressor_stock_model_" + stocksymbol)
-    return stock_model
-
-def update_model_with_fresh_data(df, model):
+def update_model_with_fresh_data(df):
     stocksymbol = df['symbol'][0]
     numeric_features = [col for col in df.columns if df[col].dtype in ['int64', 'float64'] and col != 'close']
     
@@ -78,36 +58,8 @@ def update_model_with_fresh_data(df, model):
     retrieved_model.named_steps['regressor'].partial_fit(X, y)
     
     joblib.dump(retrieved_model, stocksymbol+'_linear_regression_model.pkl')
-    mr = project.get_model_registry()
     
-    # Define the input schema using the values of X_train
-    input_schema = Schema(X.values)
 
-    # Define the output schema using y_train
-    output_schema = Schema(y)
-
-    # Create a ModelSchema object specifying the input and output schemas
-    model_schema = ModelSchema(input_schema=input_schema, output_schema=output_schema)
-
-    # Convert the model schema to a dictionary for further inspection or serialization
-    model_schema.to_dict()
-    
-    mr = project.get_model_registry()
-
-    metrics = {
-        "neg_mean_squared_error": np.mean(cross_val_score(retrieved_model, X, y, cv=5, scoring='neg_mean_squared_error'))
-    }
-
-    # Create a new model in the model registry
-    stock_model = mr.python.create_model(
-        name="regressor_stock_model_" + stocksymbol,     # Name for the model
-        metrics=metrics,                      # Metrics used for evaluation
-        model_schema=model_schema,            # Schema defining the model's input and output
-        input_example=X.sample(),       # Example input data for reference
-        description="Stock Predictor",  # Description of the model
-    )   
-    
-    stock_model.save(stocksymbol+'_linear_regression_model.pkl')
     
 
 if __name__ == '__main__':
@@ -115,12 +67,9 @@ if __name__ == '__main__':
     
     symbols = ['AAPL', 'GOOG', 'TSLA', 'AMZN', 'MSFT', 'META', 'NVDA']
     
-    project = hopsworks.login()
     start_time = time.time()   
     for symbol in symbols:
         df = get_stock_from_api(symbol)
-        upload_to_hopsworks(df, project)
-        stock_model = retrieve_model_from_hopsworks(project, symbol)
         #check if 15 seconds have passed if not wait until 15 seconds have passed
         #to avoid rate limiting of the API
         end_time = time.time()
@@ -128,8 +77,6 @@ if __name__ == '__main__':
             waiting_time = 15 - (end_time - start_time)
             time.sleep(waiting_time)
             print(f"Waiting for {waiting_time} seconds")
-        update_model_with_fresh_data(df, stock_model)
+        update_model_with_fresh_data(df)
         
         
-    
-    hopsworks.Connection.close(project)
